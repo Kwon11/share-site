29d9765ee411715caa9a7fe3c96890ee05337501
Chan Kwon <Chanthemancan@gmail.com>
2017-11-15 18:41:31 -0800

    Add more url to test

--- src/App.js
+++ src/App.js
@@ -3,13 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 // const url = "https://cngh.adj.st"
-const url = "https://share.plexchat.com/7hf57"
+// const url = "https://share.plexchat.com/7HF57"
 // const url = "http://goo.gl/xPQjhV"
+
+// GANES
+// 53c31860-b5cc-11e7-81ea-4d1634d121b8  -- public room
+// 1c520520-669f-11e7-ad69-9ba6bed4c60f  -- members room
 
 // const url = "https://app.adjust.com/?deep_link="
 // const url = "https://app.adjust.com/p5xfpy?deeplink=plexchat%3A%2F%2F%3FspaceInviteCode=7HF57";
 
+const arrayOfURL = [
+  "https://share.plexchat.com/GANES",
+  "https://share.plexchat.com/GANES/",
+  "https://share.plexchat.com/GANES/asdf",
+  "https://share.plexchat.com/GANES/53c318601ea",
+];
+
 class App extends Component {
+
+  getListOfURL () {
+    return arrayOfURL.map((element) => {
+      return (
+        <p className="App-intro">
+          <a href={element}>
+            {element}
+          </a>
+        </p>
+      )
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -17,12 +41,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <a href={url}>
-           {url}
-          </a>
-        </p>
+        {this.getListOfURL()}
       </div>
     );
   }
