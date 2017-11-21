7040ab1e18156e6cc18af744f9d1ffbf515a44f6
Chan Kwon <Chanthemancan@gmail.com>
2017-11-21 11:56:20 -0800

    Set up for staging testing on chat-east

--- src/App.js
+++ src/App.js
@@ -14,12 +14,19 @@ import './App.css';
 // const url = "https://app.adjust.com/p5xfpy?deeplink=plexchat%3A%2F%2F%3FspaceInviteCode=7HF57";
 
 const arrayOfURL = [
-  "http://192.168.7.168:3001/7HF57",
-  "http://192.168.7.168:3001/7HF57/",
-  "http://192.168.7.168:3001/7HF57/asdf",
-  "http://192.168.7.168:3001/7HF57/01dbdbe0296",
-  "https://share.plexchat.com/7HF57",
-  "https://share.plexchat.com/7HF57/53c318601ea",
+  // "http://192.168.7.168:3001/7HF57",
+  // "http://192.168.7.168:3001/7HF57/",
+  // "http://192.168.7.168:3001/7HF57/asdf",
+  // "http://192.168.7.168:3001/7HF57/01dbdbe0296",
+  // "http://192.168.7.168:3001/7HF57/01dbdbe3296",
+  // "https://share.plexchat.com/7HF57",
+  // "https://share.plexchat.com/7HF57/53c318601ea",
+  {label: 'room link', url: "http://192.168.7.168:3001/P8NL5/8815ac60cbf" },
+  {label: 'plex link', url: "http://192.168.7.168:3001/P8NL5" },
+  {label: 'bad plex link', url: "http://192.168.7.168:3001/P9NL3"},
+  {label: 'plex link with slash', url: "http://192.168.7.168:3001/P8NL5/" },
+  {label: 'bad room link (should be filtered by server)', url: "http://192.168.7.168:3001/P8NL5/2342" },
+  {label: 'bad room link (will be sent to app, should go to plex)', url: "http://192.168.7.168:3001/P8NL5/8823949ab83" },
 ];
 
 class App extends Component {
@@ -28,8 +35,11 @@ class App extends Component {
     return arrayOfURL.map((element) => {
       return (
         <p className="App-intro">
-          <a href={element}>
-            {element}
+          <div>
+           {element.label}
+          </div>
+          <a href={element.url}>
+            {element.url}
           </a>
         </p>
       )
